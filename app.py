d9ef5f0699da40128d4abe5c4c4b2c574dfa0162
Muhammad Aamish <arainzada247@gmail.com>
2026-03-05 18:39:55 +0000

    Add Result function to display a message on button click

--- app.py
+++ app.py
@@ -47,7 +47,8 @@ st.sidebar.subheader("My conversations")
 for thread_id in st.session_state["chat_threads"]:
     st.sidebar.button(thread_id)
 
-
+def Result():
+    st.write("This is the result of the button click")
 
 
 for msg in st.session_state.messages:
