843a0585d77f013157d93201fa206bd508555004
Muhammad Aamish <arainzada247@gmail.com>
2026-03-09 13:54:39 +0000

    Add a multiply function to the codebase

--- app.py
+++ app.py
@@ -47,7 +47,8 @@ st.sidebar.subheader("My conversations")
 for thread_id in st.session_state["chat_threads"]:
     st.sidebar.button(thread_id)
 
-
+def multiply(a, b):
+    return a * b
 
 for msg in st.session_state.messages:
     with st.chat_message(msg["role"]):
