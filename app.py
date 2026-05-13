9b0d1b5c7c86033bfe408dc5646f6734f1afcc2f
Muhammad Aamish <arainzada247@gmail.com>
2026-05-13 08:08:21 +0000

    Reverse the order of chat threads displayed in the sidebar

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ if st.sidebar.button("New Chat"):
 
 st.sidebar.subheader("My conversations")
 
-for thread_id in st.session_state["chat_threads"]:
+for thread_id in st.session_state["chat_threads"][::-1]:
 
     if st.sidebar.button(str(thread_id)):
         st.session_state["thread_id"] = thread_id
